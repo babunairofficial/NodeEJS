5a74b3c70d34e556733297bcd30b57efe88ae142
Babu Nair <babunairofficial@gmail.com>
2024-05-14 20:50:14 +0530

    adding multiple static files to ejs

--- EJSDIR/index.js
+++ EJSDIR/index.js
@@ -4,7 +4,8 @@ const path = require("path");
 
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, "public"))); //a default
+app.use(express.static(path.join(__dirname, "/public/css"))); //a default
+app.use(express.static(path.join(__dirname, "/public/js"))); //multiple static files can be used.
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views")) // informs the server to search the views folder in the correct directory
